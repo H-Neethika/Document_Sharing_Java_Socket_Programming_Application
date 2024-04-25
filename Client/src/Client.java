ae1875f448e602ad68311759e54b876a691681e7
TuanFaied <tuanfaied8@gmail.com>
2024-04-25 17:04:17 +0530

    update client

--- Client/src/Client.java
+++ Client/src/Client.java
@@ -65,7 +65,7 @@ public class Client {
          } else {
            try {
              FileInputStream fileInputStream = new FileInputStream(fileToSend[0].getAbsolutePath());
-             Socket socket = new Socket("localhost", 1234);
+             Socket socket = new Socket("192.168.43.102", 1234);
 
              DataOutputStream dataOutputStream = new DataOutputStream(socket.getOutputStream());
 
